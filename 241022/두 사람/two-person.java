f0f70b52005306192c7bf7b9b1ac0d61562d46c9
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-22 00:35:18 +0900

    Title: 두 사람, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241022/두 사람/two-person.java
+++ 241022/두 사람/two-person.java
@@ -3,14 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        String gender = sc.next();
+        int result = 0;
+        int cnt = 0;
 
-        if(age >= 19 && gender.equals("M")) {
+        for(int i = 0; i < 2; i ++) {
+            int age = sc.nextInt();
+            String gender = sc.next();
+
+            if(age >= 19 && gender.equals("M")) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+
+            if(result == 1){
+                cnt++;
+            }
+
+        }
+
+        if(cnt >= 1){
             System.out.println(1);
         } else {
             System.out.println(0);
         }
+
 
 
     }
