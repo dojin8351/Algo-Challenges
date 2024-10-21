829329fcbddd9cf28b755ea3d5830510930d4c9d
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-21 22:55:27 +0900

    Title: 실수 받아 그대로 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241021/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 241021/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        double n = sc.nextInt();
 
         System.out.printf("%.2f",n);
     }
