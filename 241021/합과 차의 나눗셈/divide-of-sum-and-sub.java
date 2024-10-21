24085ed3a5030054ef7d4f35fae4267b2a162ce8
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-21 23:21:24 +0900

    Title: 합과 차의 나눗셈, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241021/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = a + b;
+        int minus = a - b;
+        double result = sum%minus;
+
+        System.out.printf("%.2f",result);
+    }
+}
