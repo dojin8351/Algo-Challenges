ab2af404b0e34a4720a9577f0681f8594625cec4
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-21 22:46:10 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241021/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+import java 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("Your score is %d point.",a);
+    }
+}
