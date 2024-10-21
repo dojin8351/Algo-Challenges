da54a4e444d8069361cc29ac668be88dff94bc2e
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-21 22:55:59 +0900

    Title: 실수 받아 그대로 출력, Time: 145ms, Memory: 12MB, Status: Passed - CodeTree

--- 241021/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 241021/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextInt();
+        double n = sc.nextDouble();
 
         System.out.printf("%.2f",n);
     }
