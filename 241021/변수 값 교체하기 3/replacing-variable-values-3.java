c63295fc5c122cd618dd82d2a47bf2a0e2ab5b62
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-21 22:30:14 +0900

    Title: 변수 값 교체하기 3, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241021/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 1;
+        c = a;
+        a = b;
+        b = c;
+        System.out.printf("%d\n%d",a,b);
+
+    }
+}
