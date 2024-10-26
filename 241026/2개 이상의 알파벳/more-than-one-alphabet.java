4db0c6e7d3d690284569c2988617e24e6cb492bf
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-26 14:12:27 +0900

    Title: 2개 이상의 알파벳, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241026/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.nextLine();
+
+        System.out.println(function(A));
+
+
+    }
+
+    public static String function(String A) {
+        String[] alpList = new String[A.length()];
+        int cnt = 0;
+        String result = "";
+
+        for(int i = 0; i < A.length(); i++) {
+            char ch = A.charAt(i);
+            alpList[i] = String.valueOf(ch);
+        }
+
+        for(int i = 0; i < alpList.length; i++) {
+            for(int j = 0; j < alpList.length; j++) {
+                if(alpList[i].equals(alpList[j])) {
+                    cnt++;
+                }
+            }
+        }
+
+        if(cnt >= 2){
+            result = "Yes";
+        } else {
+            result = "No";
+        }
+
+        return result;
+    }
+}
