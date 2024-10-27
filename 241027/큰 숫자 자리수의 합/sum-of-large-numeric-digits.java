4da537c4455ded7a6e7d82b01ce48f492e9aaf1d
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-27 16:12:14 +0900

    Title: 큰 숫자 자리수의 합, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/큰 숫자 자리수의 합/sum-of-large-numeric-digits.java
+++ 241027/큰 숫자 자리수의 합/sum-of-large-numeric-digits.java
@@ -1,25 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static int function(int a) {
-        int sum = 0;
-        if(a/10 <= 1) return a%10;
-
-        return function(a/10) + a%10;
+        // 한 자리 수일 때 그대로 반환하도록 수정
+        if (a < 10) return a;
+        return function(a / 10) + a % 10;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int mulitple = 1;
 
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             int a = sc.nextInt();
             mulitple *= a;
         }
 
-
         System.out.println(function(mulitple));
-
     }
 }
