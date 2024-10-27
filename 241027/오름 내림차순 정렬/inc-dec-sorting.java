f286d4542bcd31bbba104f884bee97666d2f6bb8
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-27 18:31:06 +0900

    Title: 오름 내림차순 정렬, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/오름 내림차순 정렬/inc-dec-sorting.java
+++ 241027/오름 내림차순 정렬/inc-dec-sorting.java
@@ -14,8 +14,13 @@ public class Main {
         }
 
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
         Arrays.sort(arr, Collections.reverseOrder());
-        System.out.println(Arrays.toString(arr));
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
