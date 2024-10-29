e8e43d2e9958bc23fdc5783e7e855c85ea7ebf2e
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-29 14:09:22 +0900

    Title: k번째로 신기한 문자열, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        String text = sc.next();
+
+
+        System.out.println(equals(n, a, text));
+
+    }
+
+    public static Object equals(int n, int a, String text) {
+        List arr = new ArrayList();
+
+        for(int i = 0; i < n; i++){
+            String word = sc.next();
+            if(word.contains(text)){
+                arr.add(word);
+            }
+        }
+
+        Collections.sort(arr);
+
+        return arr.get(a-1);
+    }
+}
