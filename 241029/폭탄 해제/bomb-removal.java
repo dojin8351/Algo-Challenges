3c0fcc96e81d46d6eccc7c92aac1110532437f2b
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-10-29 16:09:36 +0900

    Title: 폭탄 해제, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/폭탄 해제/bomb-removal.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String code = sc.next();
+        char color = sc.next().charAt(0);
+        int time = sc.nextInt();
+
+        Result result = new Result(code, color, time);
+        System.out.println(result);
+    }
+}
+
+class Result {
+
+    String code;
+    char color;
+    int time;
+
+    Result(String code, char color, int time) {
+        this.code = code;
+        this.color = color;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "code : " + code + "\ncolor : " + color + "\ntime : " + time;
+    }
+}
