0649ff040e8f40427b8678127420abd3262789a8
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-11-04 23:04:45 +0900

    Title: 십진수와 이진수 2, Time: 133ms, Memory: 9MB, Status: Passed - CodeTree

--- 241104/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 241104/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -6,9 +6,40 @@ public class Main {
 
         String number = sc.next();
 
-        int decimal =Integer.parseInt(number,2) * 17;
-        System.out.println(Integer.toBinaryString(decimal));
-        
+        int decimal = decimalSystem(number) * 17;
+        binarySystem(decimal,2);
+
+
     }
 
+    public static void binarySystem(int number, Integer binaryNum){
+
+        List<Integer> elm = new ArrayList<>();
+
+        while(true){
+            if(number == 0 || number < binaryNum){
+                elm.add(number);
+                break;
+            }
+
+            elm.add(number % binaryNum);
+            number /= binaryNum;
+        }
+
+        for(int i = elm.size() - 1; i >= 0; i--){
+            System.out.print(elm.get(i));
+        }
+    }
+
+
+    public static int decimalSystem(String number){
+        List<Integer> elm = new ArrayList<>();
+
+        char[] ch = number.toCharArray();
+        int decimal = 0;
+        for(int i = 0; i < ch.length; i++){
+            decimal += ((int) ((ch[i] -'0') * Math.pow(2,ch.length - i-1)));
+        }
+        return decimal;
+    }
 }
