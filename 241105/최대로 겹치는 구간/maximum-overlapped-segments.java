dcaf8063af8d3e32e3d14fdcab79c7c60839fe18
dojin8351 <163515795+dojin8351@users.noreply.github.com>
2024-11-05 00:20:50 +0900

    Title: 최대로 겹치는 구간, Time: 123ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 241105/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241105/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); 
+        int N = sc.nextInt();
 
         int[] diff = new int[100];
 
